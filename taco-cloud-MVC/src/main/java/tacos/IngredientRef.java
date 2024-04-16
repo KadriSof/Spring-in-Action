4ccb2c90707a60b35a87813b5701c0927eca067b
Mohamed Sofiene Kadri <kadrisofyen@gmail.com>
2024-04-16 13:22:42 +0100

    Add files via upload

---
+++ taco-cloud-MVC/src/main/java/tacos/IngredientRef.java
@@ -0,0 +1,14 @@
+package tacos;
+
+import org.springframework.data.relational.core.mapping.Table;
+
+@Table
+public class IngredientRef {
+
+	private final String ingredient;
+
+	public Object getIngredient() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
